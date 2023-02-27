47534e07e77ccdf9848b0fd250cd3d18ec79219f
sjyoo1572 <sjyoo1572@gmail.com>
2023-02-27 15:02:06 +0900

    실시간 거래내역 삭제 & 거래종목 최대치 삭제

--- pytrader.py
+++ pytrader.py
@@ -71,7 +71,7 @@ class MyWindow(QMainWindow, form_class):
         self.lineEdit_9.textChanged.connect(self.price_change)
         self.pushButton_6.clicked.connect(self.kiwoom._condition_search)
         
-        self.lineEdit_11.textChanged.connect(self.enable_pushButton_6)
+        self.lineEdit_12.textChanged.connect(self.enable_pushButton_6)
         
         self.gudoc_status = 0
         self.ticker_list = []
@@ -490,6 +490,7 @@ class MyWindow(QMainWindow, form_class):
     #익절기준 변경점
    # def profit_percent(self):
         #self.take_profit = float(self.lineEdit_8.text())
+
 
 
 
